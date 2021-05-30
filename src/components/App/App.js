6933d4828e830c775833204f66fb919d11063daf
Doaa Mahely <Doaa Mahely>
2021-05-30 23:49:51 +0400

    Fix applying theme on body

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "./App.css";
 import { Header } from "../Header/Header";
 import { OverviewContainer } from "../OverviewContainer/OverviewContainer";
@@ -9,30 +9,29 @@ const App = () => {
   const [isDark, setIsDark] = useState(false);
   const theme = { isDark, setIsDark };
   return (
-    <div
-      className={cx(
-        "App--container",
-        { "theme-light": !isDark },
-        { "theme-dark": isDark }
-      )}
-    >
-      <ThemeContext.Provider value={theme}>
-        <Header />
-      </ThemeContext.Provider>
-      <OverviewContainer />
-      @nathanf 1987 Followers 12 Today @nathanf 1044 Followers 99 Today
-      @realnathanf 11k Followers 1099 Today Nathan F. 8239 Subscribers 144 Today
-      Overview - Today Page Views 87 3% Likes 52 2% Likes 5462 2257% Profile
-      Views 52k 1375% Retweets 117 303% Likes 507 553% Likes 107 19% Total Views
-      1407 12%
-      <div>
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Your Name Here</a>.
+    <main className={cx({ "theme-light": !isDark }, { "theme-dark": isDark })}>
+      <div
+        className={cx(
+          "App--container",
+          { "theme-light": !isDark },
+          { "theme-dark": isDark }
+        )}
+      >
+        <ThemeContext.Provider value={theme}>
+          <Header />
+        </ThemeContext.Provider>
+        <OverviewContainer />
+        @nathanf 1987 Followers 12 Today @nathanf 1044 Followers 99 Today
+        @realnathanf 11k Followers 1099 Today Nathan F. 8239 Subscribers 144
+        <div>
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Your Name Here</a>.
+        </div>
       </div>
-    </div>
+    </main>
   );
 };
 
