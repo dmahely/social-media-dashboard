071ebc95d9df42dcd399b3ab60a5a54bffd2fe96
Doaa Mahely <Doaa Mahely>
2021-06-01 21:52:41 +0400

    Add styling to toggle

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,11 @@ import "./Header.css";
 import { ThemeContext } from "../../ThemeContext";
 
 const Header = () => {
-  const { isDark, setIsDark } = useContext(ThemeContext);
+  const { setIsDark } = useContext(ThemeContext);
 
-  const handleClick = (e) => setIsDark((isDark) => !isDark);
+  const handleClick = (e) => {
+    setIsDark((isDark) => !isDark);
+  };
   return (
     <div className="Header--container">
       <div className="Header--inner-container">
@@ -13,14 +15,20 @@ const Header = () => {
         <span className="Header--follower-count">Total Followers: 23,004</span>
       </div>
       <hr className="Header--divider" />
-      <label className="Header--toggle-label">
-        Dark Mode{" "}
+      <div className="Header--toggle-container">
+        <span className="Header--toggle-label" aria-hidden="true">
+          Dark Mode
+        </span>
         <input
           className="Header--toggle"
+          id="Header--toggle"
           onClick={handleClick}
           type="checkbox"
         />
-      </label>
+        <label className="Header--toggle-label-hidden" for="Header--toggle">
+          Dark Mode
+        </label>
+      </div>
     </div>
   );
 };
